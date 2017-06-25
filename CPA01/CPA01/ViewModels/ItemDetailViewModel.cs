d7abfd6563612e9c19c0926fcdfddc314fda3141
Risto Päärni <risto.paarni@lifetime.fi>
2017-06-25 08:58:47 +0300

    Add project files.

---
+++ CPA01/CPA01/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CPA01.Models;
+
+namespace CPA01.ViewModels
+{
+	public class ItemDetailViewModel : BaseViewModel
+	{
+		public Item Item { get; set; }
+		public ItemDetailViewModel(Item item = null)
+		{
+			Title = item.Text;
+			Item = item;
+		}
+
+		int quantity = 1;
+		public int Quantity
+		{
+			get { return quantity; }
+			set { SetProperty(ref quantity, value); }
+		}
+	}
+}
---
+++ CPA01/CPA01/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace CPA01.Views
+{
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
